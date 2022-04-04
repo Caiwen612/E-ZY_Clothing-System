e32a574875395071d00b9d06cc79216fcc27030f
Caiwen612 <chaiboon0612@gmail.com>
2022-04-04 16:12:13 +0800

    Update validation

--- src/utility/Validation.java
+++ src/utility/Validation.java
@@ -12,7 +12,7 @@ public class Validation {
     //Check Menu option
    public static void validOption(int option, int lowerLimit, int upperLimit) throws ValidationException {
         int intLength = String.valueOf(option).length();// convert integer to String and get length of the String
-        if (intLength == 2) {
+        if (intLength >= 2) {
             throw new ValidationException(Font.useFont(Font.BOLD_RED, "Please don't key in more than 2 digits."));
         }
 
